da1ad443f0b7562836fc89622b3b1805aea40feb
joannechen1223 <joannechen1223@gmail.com>
2024-04-20 20:10:45 -0400

    feat: init create-react-app and add linter

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'react-app', 'react-app/jest'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+const config = {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'es5',
+};
+
+module.exports = config;
